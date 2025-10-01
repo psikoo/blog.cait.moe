bcf081308b6544f8e53df303012e7cfe86557416
psikoo <hugohernandezmuelas03@gmail.com>
2025-10-01 10:37:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ echo "> blog"
 git pull
 chmod +x ./build.sh
 fileNum=$(sudo ls ./entries | wc -l)
-sudo touch ./entries/counter.txt
-sudo chmod +666 ./entries/counter.txt
-sudo echo $fileNum > ./entries/counter.txt
+touch ./entries/counter.txt
+chmod +666 ./entries/counter.txt
+echo $fileNum > ./entries/counter.txt
 rm -rf ./out
 mkdir ./out
 cp -r ./ ./out/
